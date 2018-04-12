28214511a1570ec2b7d89dfb699ca06833434fee
Joma Ejercito <jomaejercito@gmail.com>
2018-04-12 21:58:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |details|
-        Ship.new(details)
+      params[:pirate][:ships].each do |info|
+        Ship.new(info)
       end
       @ships = Ship.all
 
